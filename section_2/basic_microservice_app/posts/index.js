bddfcd4e395be3286279ac41f2fc13bf4af20684
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-01 21:27:18 +0530

    posts microservice elements

---
+++ section_2/basic_microservice_app/posts/index.js
@@ -0,0 +1,24 @@
+const express = require("express")
+const bodyParser = require("body-parser")
+const { randomBytes } = require("crypto")
+const app = express()
+app.use(bodyParser.json())
+
+const posts = {}
+
+app.get("/posts", (request, response) => {
+    response.send(posts)
+})
+
+app.post("/posts", (request, response) => {
+    const id = randomBytes(4).toString('hex')
+    const { title } = request.body;
+    posts[id] = {
+        id, title
+    };
+    response.status(201).send(posts[id])
+})
+
+app.listen(4000, () => {
+    console.log("Listening on port 4000")
+})
