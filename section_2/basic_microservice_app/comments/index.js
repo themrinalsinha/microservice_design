d2de26e8fdcd1882ec78eee66e6fa5050ca990a6
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-03 19:56:00 +0530

    comment service emmiting data

--- section_2/basic_microservice_app/comments/index.js
+++ section_2/basic_microservice_app/comments/index.js
@@ -2,6 +2,7 @@ const express = require('express')
 const bodyParser = require('body-parser')
 const { randomBytes } = require('crypto')
 const cors = require('cors')
+const axios = require('axios')
 
 const app = express()
 app.use(bodyParser.json())
@@ -13,12 +14,21 @@ app.get('/posts/:id/comments', (request, response) => {
     response.send(commentsByPostId[request.params.id] || [])
 })
 
-app.post('/posts/:id/comments', (request, response) => {
+app.post('/posts/:id/comments', async (request, response) => {
     const commentId = randomBytes(4).toString('hex')
     const { content } = request.body
 
     const comments = commentsByPostId[request.params.id] || []
     comments.push({ id: commentId, content })
+
+    await axios.post('http://localhost:4005/events', {
+        type: "CommentCreated",
+        data: {
+            id: commentId,
+            content,
+            postId: request.params.id
+        }
+    })
 
     commentsByPostId[request.params.id] = comments
     response.status(201).send(comments)
