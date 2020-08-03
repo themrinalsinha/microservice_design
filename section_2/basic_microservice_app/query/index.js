c56383e720875fd713aacfabcb33d5d1eb0c243b
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-03 22:10:20 +0530

    creating our query service

--- section_2/basic_microservice_app/query/index.js
+++ section_2/basic_microservice_app/query/index.js
@@ -6,12 +6,28 @@ const app = express()
 app.use(bodyParser.json())
 app.use(cors())
 
-app.get("/posts", (request, response) => {
+const posts = {}
 
+app.get("/posts", (request, response) => {
+    response.send(posts)
 })
 
 app.post("/events", (request, response) => {
+    const { type, data } = request.body
+    if (type === 'PostCreated') {
+        const { id, title } = data
+        posts[id] = { id, title, comments: [] }
+    }
 
+    if (type === 'CommentCreated') {
+        const { id, content, postId } = data
+        const post = posts[postId]
+        post.comments.push({id, content})
+    }
+
+    console.log(posts)
+
+    response.send({})
 })
 
 app.listen(4002, () => {
