9cb28622ee1e17b199ceaf607d3d1d32a723739a
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-03 09:03:21 +0530

    setting up comments

--- section_2/basic_microservice_app/client/src/PostList.jsx
+++ section_2/basic_microservice_app/client/src/PostList.jsx
@@ -1,5 +1,6 @@
 import React, { useState, useEffect } from 'react'
 import axios from 'axios'
+import CommentCreate from './CommentCreate'
 
 export default() => {
     const [posts, setPosts] = useState({})
@@ -20,7 +21,8 @@ export default() => {
             key={post.id}
         >
             <div className="card-body">
-            <h3>{post.title}</h3>
+                <h3>{post.title}</h3>
+                <CommentCreate postId={post.id} />
             </div>
         </div>
     })
