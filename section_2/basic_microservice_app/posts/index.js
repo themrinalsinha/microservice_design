f6d05784aa1bfc9722f71db8287e666aab0cbab0
Mrinal Sinha <themrinalsinha@gmail.com>
2020-08-03 19:01:15 +0530

    posts app emitting events

--- section_2/basic_microservice_app/posts/index.js
+++ section_2/basic_microservice_app/posts/index.js
@@ -1,5 +1,6 @@
 const express = require("express")
 const bodyParser = require("body-parser")
+const axios = require("axios")
 const { randomBytes } = require("crypto")
 const cors = require('cors')
 
@@ -13,12 +14,21 @@ app.get("/posts", (request, response) => {
     response.send(posts)
 })
 
-app.post("/posts", (request, response) => {
+app.post("/posts", async (request, response) => {
     const id = randomBytes(4).toString('hex')
     const { title } = request.body;
     posts[id] = {
         id, title
     };
+
+    // creating event
+    await axios.post('http://localhost:4005/events', {
+        type: 'PostCreated',
+        data: {
+            id, title
+        }
+    })
+
     response.status(201).send(posts[id])
 })
 
