ec1756659bf31fa8ec8f699906bc03d77b55ba33
Mrinal Sinha <themrinalsinha@gmail.com>
2020-09-24 09:36:11 +0530

    deployment updated

--- mini-app/posts/index.js
+++ mini-app/posts/index.js
@@ -41,6 +41,7 @@ app.post("/events", (request, response) => {
 
 // running the post micro service instance on port 4000
 app.listen(4000, () => {
+    console.log("latest")
     console.log("v0.0.2")
     console.log("Listening on port 4000")
 })
